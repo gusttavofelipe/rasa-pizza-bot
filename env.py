8876173622797d96ad14ad0207f0d8f28cd85d0f
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-12-11 12:36:58 -0300

    refactor: restructure configuration file

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-ACCOUNT_SID = os.getenv("ACCOUNT_SID")
-AUTH_TOKEN = os.getenv("AUTH_TOKEN")
-WHATSAPP_TO = os.getenv("WHATSAPP_TO")
-WHATSAPP_FROM = os.getenv("WHATSAPP_FROM")
-TEMPLATE_SID = os.getenv("TEMPLATE_SID")
