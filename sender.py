d6ba0c444e01407d70f5be76ff34aaebca6284d4
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-12-11 12:52:27 -0300

    refactor: update sender

--- sender.py
+++ sender.py
@@ -1,21 +1,27 @@
 from twilio.rest import Client
-import env
+from pizzabot.config import settings
 
 
-account_sid = env.ACCOUNT_SID
-auth_token = env.AUTH_TOKEN
-whatsapp_from = env.WHATSAPP_FROM
-whatsapp_to = env.WHATSAPP_TO
-template = env.TEMPLATE_SID
+class Twilio:
+    """"""
+
+    def client(self):
+        """"""
+        return Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
+
+    def post_message(self, template_sid: str, tracker=None,):
+        """"""
+
+        try:
+            MESSAGE = self.client().messages.create(
+                from_=settings.WHATSAPP_FROM,
+                to=settings.WHATSAPP_TO,
+                content_sid=template_sid,
+            )
+            print("MENSAGEM ENVIADA:", MESSAGE.sid)
+        except Exception as exc:
+            print("OCORREU UM ERRO:", exc)
 
 
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    from_=whatsapp_from,
-    to=whatsapp_to,
-    content_sid=template,
-)
-
-
-print(f"MESSAGE SID: {message.sid}")
+twilio = Twilio()
+twilio.post_message(settings.TEMPLATE_SID)
