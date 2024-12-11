2da8f3e44285cc467b4c8ffab91061013e530ec6
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-12-11 12:46:31 -0300

    feat: add SttGreet action class

--- pizzabot/actions/actions.py
+++ pizzabot/actions/actions.py
@@ -1,3 +1,22 @@
-# from typing import Any, Text, Dict, List
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from messaging.twilio_ import twilio
+from rasa_sdk import Action
+from rasa_sdk.events import EventType
+from rasa_sdk.types import DomainDict
+from typing import List, Text
+from pizzabot.config import settings
+
+
+class SttGreet(Action):
+    def name(self) -> Text:
+        return "action_stt_greet"
+
+    def run(
+        self,
+        dispatcher,
+        tracker,
+        domain: DomainDict,
+    ) -> List[EventType]:
+        twilio.post_message(
+            template_sid=settings.TEMPLATE_SID, action_tracker=tracker
+        )
+        return []
--- pizzabot/actions/twilio_connector.py
+++
@@ -1,3 +0,0 @@
-# from typing import Text, Dict, Any
-# from rasa.core.channels.twilio import TwilioOutput, InputChannel
-# from twilio.rest import Client
